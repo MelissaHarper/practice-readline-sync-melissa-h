419489c1414ffc65716a4bafc9b383f9a9973d9c
Harper <HarperMelissaM@gmail.com>
2025-03-17 14:37:22 -0500

    initial commit

---
+++ practice_readline_sync.js
@@ -0,0 +1,18 @@
+const ask = require('readline-sync');
+
+//Practice- 4 Values, Data Types, and Operations
+
+let name = ask.question("Please enter your first name: ");
+
+console.log("Hello, " + name);
+
+let dataType = ask.question("Name one data type used in JavaScript: ");
+let operator = ask.question("Enter one mathematical operator that can be used in JavaScript coding: ");
+let bool = ask.question("True or false, the output of console.log(\"5\" + \"2\") is 7? ");
+let output = ask.questionInt("Using only numbers, what do you think the output of the above code will be? ")
+let conversion = ask.question("Does the above equation use conversion? ")
+
+
+console.log("The data type you chose is " + dataType + ". The operator you chose is " + operator + 
+    ". You said the given output for the code provided is " + bool + ". You said the output should be " 
+    + output + ". You anserred " + conversion + " to the conversion question.");
